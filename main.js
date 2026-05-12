b4819ce994c088c9c73f91812c7203386df00f3e
Ayşe Ayhan <161196580+AyseAyhann@users.noreply.github.com>
2026-05-12 14:31:36 +0300

    Axios added, all completed

--- main.js
+++ main.js
@@ -28,9 +28,16 @@ console.log(ipAdresim);
 
   Bu fonksiyonda return ettiğiniz veri, Adım 2'de oluşturacağınız component'de argüman olarak kullanılıyor. Bu yüzden, veride hangi key-value çiftleri olduğunu inceleyin.
 */
-
+/*await axios.get("url") */
 async function getData() {
-  /* kodlar buraya */
+  try {
+    const response = await axios.get(
+      `https://apis.ergineer.com/ipgeoapi/${ipAdresim}`,
+    );
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 /*
@@ -42,8 +49,8 @@ async function getData() {
 
 	<div class="card">
     <img src={ülke bayrağı url} />
-    <div class="card-info">
-      <h3 class="ip">{ip adresi}</h3>
+    <div class="card-info"> 
+    <h3 class="ip">{ip adresi}</h3>
       <p class="ulke">{ülke bilgisi (ülke kodu)}</p>
       <p>Enlem: {enlem} Boylam: {boylam}</p>
       <p>Şehir: {şehir}</p>
@@ -54,8 +61,49 @@ async function getData() {
   </div>
 */
 
-function cardOlustur(/* kodlar buraya */) {
-  /* kodlar buraya */
+function cardOlustur(myObj) {
+  const div = document.createElement("div");
+  div.classList.add("card");
+
+  const img = document.createElement("img");
+  img.src = `https://flaglog.com/codes/standardized-rectangle-120px/${myObj.ülkeKodu}.png`;
+  div.appendChild(img);
+
+  const div2 = document.createElement("div");
+  div2.classList.add("card-info");
+  div.appendChild(div2);
+
+  const h3 = document.createElement("h3");
+  h3.classList.add("ip");
+  h3.textContent = myObj.sorgu;
+  div2.appendChild(h3);
+
+  const pUlke = document.createElement("p");
+  pUlke.classList.add("ulke");
+  pUlke.textContent = `${myObj.ülke} (${myObj.ülkeKodu})`;
+  div2.appendChild(pUlke);
+
+  const pKonum = document.createElement("p");
+  pKonum.textContent = `Enlem: ${myObj.enlem} - Boylam: ${myObj.boylam}`;
+  div2.appendChild(pKonum);
+
+  const pSehir = document.createElement("p");
+  pSehir.textContent = `Şehir: ${myObj.bölgeAdı}`;
+  div2.appendChild(pSehir);
+
+  const pSaat = document.createElement("p");
+  pSaat.textContent = `Saat dilimi: ${myObj.saatdilimi}`;
+  div2.appendChild(pSaat);
+
+  const pPara = document.createElement("p");
+  pPara.textContent = `Para birimi: ${myObj.parabirimi}`;
+  div2.appendChild(pPara);
+
+  const pIsp = document.createElement("p");
+  pIsp.textContent = `ISP: ${myObj.isp}`;
+  div2.appendChild(pIsp);
+
+  return div;
 }
 
 // Buradan sonrasını değiştirmeyin, burası yazdığınız kodu sayfaya uyguluyor.
